e34f030b2867b00c24c3a60d74700b03b0ed96e9
miiitanii <miiitanii.hiroki@gmail.com>
2021-07-21 05:42:50 +0900

    Make Post Spread

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # chrome-extensions-formyself
  自分用のGoogle Chrome拡張機能です
+
+## test
+公式のGetting startedページのサンプルです。
+
+[https://developer.chrome.com/docs/extensions/mv3/getstarted/](https://developer.chrome.com/docs/extensions/mv3/getstarted/)
+
+
+## Post Spread
+開いているタブのタイトルとURLを簡単にPOSTリクエストできる拡張機能です。
+
++でパラメータを追加したり、xで不要なパラメータを削除したりもできます。
+
+スプレッドシートに向けてPOSTするつもりで作成したのでPost Spreadです。
--- src/test/assets/js/script.js
+++ src/test/assets/js/script.js
@@ -1,31 +1,92 @@
-// // Initialize button with user's preferred color
-// let changeColor = document.getElementById("changeColor");
+// elements
+let popup = document.querySelector('.p-popup');
+let popupList = document.querySelector('.p-popup__list');
 
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
+// input
+let urlInput = document.querySelector('.js-url');
+// POSTURLがストレージにある場合取得
+chrome.storage.local.get(["postUrl"], (result) => {
+  if(result.postUrl){
+    urlInput.value = result.postUrl
+  }
+});
 
-// // When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// btn
+let plusBtn = document.querySelector('.js-plus');
+let postBtn = document.querySelector('.js-post');
 
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-// });
-
-// // The body of this function will be executed as a content script inside the
-// // current page
-// function setPageBackgroundColor() {
-//   chrome.storage.sync.get("color", ({ color }) => {
-//     document.body.style.backgroundColor = color;
-//   });
+let addForm = (name = "", value = "") => {
+  const _form = document.createElement("li");
+  _form.classList.add("p-popup__item");
+  _form.innerHTML = `
+    <div class="p-popup__head">
+      <input type="text" class="js-name" value="${name}">
+    </div>
+    <div class="p-popup__body">
+      <input type="text" class="js-value" value="${value}">
+    </div>
+    <div class="p-popup__close">
+      <button class="p-popup__close-btn js-close">x</button>
+    </div>
+  `
+  _form.querySelector('.p-popup__close-btn').addEventListener("click", function() {
+    this.closest(".p-popup__item").remove();
+  });
+  popupList.appendChild(_form);
 }
 
+function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = chrome.tabs.query(queryOptions);
+  return tab;
+}
 
+async function initForm() {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  addForm("title", tab.title);
+  addForm("url", tab.url);
+}
 
+initForm();
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-// });
+plusBtn.addEventListener("click", () => {
+  addForm();
+});
+
+postBtn.addEventListener("click", () => {
+  // POSTURLを保存
+  let postUrl = urlInput.value;
+  chrome.storage.local.set({ "postUrl": postUrl });
+  // POST処理
+  popup.dataset.state = "post";
+
+  postBtn.innerHTML = "POST中……";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", postUrl, true);
+  xhr.onreadystatechange = function() {
+    // リクエスト終了時
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      var _status = xhr.status;
+      postBtn.innerHTML = `HTTP STATUS：${_status}`;
+      popup.dataset.state = "result"
+      if (_status >= 200 && _status < 400) {
+        // 正常終了
+        if(_status == 200) {
+          popup.dataset.status = "success";
+        } else {
+          popup.dataset.status = "caution";
+        }
+      } else {
+        // エラー
+        popup.dataset.status = "error";
+      }
+    }
+    setTimeout(function(){
+      popup.dataset.state = "";
+      popup.dataset.status = "";
+      postBtn.innerHTML = "POST";
+    },1500);
+  };
+  xhr.send();
+});
